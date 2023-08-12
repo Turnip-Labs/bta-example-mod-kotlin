88355160fe8d8f565bab9127841cde79902821c7
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-08-12 18:58:03 +0200

    Made things more proper.

--- src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
+++ src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
@@ -5,13 +5,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static turniplabs.halplibe.HalpLibe.LOGGER;
+import turniplabs.examplemod.ExampleMod;
 
 @Mixin(value = GuiMainMenu.class, remap = false)
 public class GuiMainMenuMixin {
     @Inject(method = "<clinit>", at = @At("HEAD"))
     private static void examplemod$onClinit(CallbackInfo ci) {
-        LOGGER.info("This line is printed by the example Mixin!");
+        ExampleMod.LOGGER.info("This line is printed by the example Mixin!");
     }
 }
--- src/main/kotlin/turniplabs/examplemod/ExampleMod.kt
+++ src/main/kotlin/turniplabs/examplemod/ExampleMod.kt
@@ -1,16 +1,24 @@
 package turniplabs.examplemod
 
+import net.fabricmc.api.ModInitializer
+import net.java.games.input.Component.Identifier.Key.T
+import org.checkerframework.common.returnsreceiver.qual.This
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-val MODID: String = "examplemod"
-val LOGGER: Logger = LoggerFactory.getLogger(MODID)
+object ExampleMod: ModInitializer {
+    @JvmField
+    val MODID: String = "examplemod"
 
-@Suppress("unused")
-fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
+    @JvmField
+    val LOGGER: Logger = LoggerFactory.getLogger(MODID)
 
-    LOGGER.info("Hello Fabric world!")
+    override fun onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!")
+    }
+
 }

4aab6d5bdd089de829a8b6a888fabbedf4a8bdb4
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2025-03-23 14:59:22 +1300

    Updated to 7.3_01!

--- src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
+++ src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
@@ -1,13 +1,13 @@
 package turniplabs.examplemod.mixin;
 
-import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.ScreenMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import turniplabs.examplemod.ExampleMod;
 
-@Mixin(value = GuiMainMenu.class, remap = false)
+@Mixin(value = ScreenMainMenu.class, remap = false)
 public class GuiMainMenuMixin {
     @Inject(method = "<clinit>", at = @At("HEAD"))
     private static void examplemod$onClinit(CallbackInfo ci) {
